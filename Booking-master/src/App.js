9eef3e3ad83cd829187c00dbf178581f4c1f4f00
AsadSi <91189953+AsadSi@users.noreply.github.com>
2022-11-17 12:30:30 +0100

    Add files via upload

---
+++ Booking-master/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header1 from './components/Header1';
+import Navbar from './components/Navbar';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Header1 />
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
